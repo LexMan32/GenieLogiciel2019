385d887ba91fc497e6b0059612aefa5ce9e00fe3
Alexandre Morel - DIVTEC <alexandre.morel@divtec.ch>
2019-09-18 12:04:10 +0200

    Ajoutez des fichiers projet.

---
+++ LoggerSingleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
