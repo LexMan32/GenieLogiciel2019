5583edd2ce875ab657d791e7e788a9ea0ea8358c
Alexandre Morel - DIVTEC <alexandre.morel@divtec.ch>
2019-10-02 12:36:14 +0200

    Déclaration Voiture et Ferrari

---
+++ PresentationDecorateur/Voitures/Ferrari.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationDecorateur.Voitures
+{
+    class Ferrari : Voiture
+    {
+        public override double Prix { get { return 749.99; } }
+
+        public override string Modele { get { return "308"; } }
+    }
+}
---
+++ PresentationDecorateur/Voitures/Voiture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationDecorateur.Voitures
+{
+    public abstract class Voiture
+    {
+        public abstract double Prix { get; }
+
+        public abstract string Modele { get; }
+    }
+}
