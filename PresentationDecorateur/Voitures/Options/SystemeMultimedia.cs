016386f4b6aa862b846467cd871fe7f65270e96e
Alexandre Morel - DIVTEC <alexandre.morel@divtec.ch>
2019-10-23 12:32:52 +0200

    Ajout d'options

--- PresentationDecorateur/Voitures/Options/SystemeMultimedia.cs
+++ PresentationDecorateur/Voitures/Options/SystemeMultimedia.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace PresentationDecorateur.Voitures.Options
 {
-    class SystemeMultimedia
+    class SystemeMultimedia : Options
     {
+        public override double Tarif { get { return 735.00; } }
+
+        public override string Description { get { return "Toit ouvrant"; } }
+
+        public SystemeMultimedia(Voiture voiture) : base(voiture) { }
     }
 }
--- PresentationDecorateur/Voitures/Options/ToitOuvrant.cs
+++ PresentationDecorateur/Voitures/Options/ToitOuvrant.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace PresentationDecorateur.Voitures.Options
 {
-    class ToitOuvrant
+    class ToitOuvrant : Options
     {
+        public override double Tarif { get { return 1550.00; } }
+
+        public override string Description { get { return "Toit ouvrant"; } }
+
+        public ToitOuvrant(Voiture voiture) : base(voiture) { }
     }
 }
